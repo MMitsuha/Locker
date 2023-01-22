49b1ff210d108134307c18706b1040a188204046
宮水_五葉 <caizhe666@qq.com>
2023-01-22 14:33:06 +0800

    [FIX] x86 build fix

--- Locker/stdafx.h
+++ Locker/stdafx.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #define _CRT_SECURE_NO_WARNINGS 1
 //#include <iostream>
 #include <Windows.h>
@@ -7,3 +8,15 @@
 //#include <fmt/xchar.h>
 #include <lazy_importer.hpp>
 #include "XorString.h"
+
+void* __cdecl memset(
+	_Out_writes_bytes_all_(_Size) void* _Dst,
+	_In_                          int    _Val,
+	_In_                          size_t _Size
+)
+{
+	register unsigned char* ptr = (unsigned char*)_Dst;
+	while (_Size-- > 0)
+		*ptr++ = _Val;
+	return _Dst;
+}
